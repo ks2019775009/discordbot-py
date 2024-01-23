a1be366ee7a3c6865430811407380a95bcd4b56b
Sumin_Kim <ks2019775009>
2024-01-24 00:10:37 +0900

    onlinetest

--- discordbot.py
+++ discordbot.py
@@ -2,8 +2,6 @@ from selenium.webdriver.chrome.options import *
 from selenium import webdriver
 from discord.ext import commands
 import discord
-import requests
-from bs4 import BeautifulSoup
 import time
 import os
 
